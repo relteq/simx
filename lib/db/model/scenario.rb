b7a27ad5290e6be893d1f78c4cb1da9e12ba570b
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-04-18 13:46:04 -0700

    fixed typos

--- lib/db/model/scenario.rb
+++ lib/db/model/scenario.rb
@@ -7,7 +7,7 @@ module Aurora
     many_to_one :srp_set,   :class => SplitRatioProfileSet
     many_to_one :cp_set,    :class => CapacityProfileSet
     many_to_one :dp_set,    :class => DemandProfileSet
-    many_to_one :ic_set,    :class => IntersectionControllerSet
+    many_to_one :ic_set,    :class => InitialConditionSet
     many_to_one :event_set, :class => EventSet
     many_to_one :ctrl_set,  :class => ControllerSet
 
--- lib/db/model/tln.rb
+++ lib/db/model/tln.rb
@@ -7,11 +7,11 @@ module Aurora
     # editing a set. It doesn't restrict which networks can be used with
     # the set in a scenario.
     k = :network_id
-    one_to_many :srp_set,   :class => SplitRatioProfileSet,      :key => k
-    one_to_many :cp_set,    :class => CapacityProfileSet,        :key => k
-    one_to_many :dp_set,    :class => DemandProfileSet,          :key => k
-    one_to_many :ic_set,    :class => IntersectionControllerSet, :key => k
-    one_to_many :event_set, :class => EventSet,                  :key => k
-    one_to_many :ctrl_set,  :class => ControllerSet,             :key => k
+    one_to_many :srp_set,   :class => SplitRatioProfileSet, :key => k
+    one_to_many :cp_set,    :class => CapacityProfileSet,   :key => k
+    one_to_many :dp_set,    :class => DemandProfileSet,     :key => k
+    one_to_many :ic_set,    :class => InitialConditionSet,   :key => k
+    one_to_many :event_set, :class => EventSet,             :key => k
+    one_to_many :ctrl_set,  :class => ControllerSet,        :key => k
   end
 end
