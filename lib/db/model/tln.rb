033cb44497a595a7a1f1ef49cae75fcbcec593f9
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-04-28 14:57:31 -0700

    minor edits

--- lib/db/model/tln.rb
+++ lib/db/model/tln.rb
@@ -3,7 +3,7 @@ module Aurora
     many_to_one :project
 
     one_to_many :scenarios, :key => :network_id
-    one_to_many :networks, :key => :network_id
+    one_to_many :networks,  :key => :network_id
 
     # The following relations are so we know which network to use when
     # editing a set. It doesn't restrict which networks can be used with
@@ -12,7 +12,7 @@ module Aurora
     one_to_many :srp_set,   :class => SplitRatioProfileSet, :key => k
     one_to_many :cp_set,    :class => CapacityProfileSet,   :key => k
     one_to_many :dp_set,    :class => DemandProfileSet,     :key => k
-    one_to_many :ic_set,    :class => InitialConditionSet,   :key => k
+    one_to_many :ic_set,    :class => InitialConditionSet,  :key => k
     one_to_many :event_set, :class => EventSet,             :key => k
     one_to_many :ctrl_set,  :class => ControllerSet,        :key => k
   end
