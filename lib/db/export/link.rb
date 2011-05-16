63fe7b3def85d196626fcbf96c54c40c0a48c2f8
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-05-16 12:04:52 -0700

    complete export node and link

--- lib/db/export/link.rb
+++ lib/db/export/link.rb
@@ -1,8 +1,14 @@
 module Aurora
   class Link
     def build_xml(xml)
-      xml.link(:id => id, :name => name, :type => self[:type]) {
+      xml.link(:id => id, :name => name, :type => self[:type],
+               :lanes => lanes, :length => length) {
         xml.description description unless description.empty?
+        xml.begin(:node_id => begin_node.id)
+        xml.end(:node_id => end_node.id)
+        xml << fd
+        xml.dynamics(:type => dynamics)
+        xml.qmax { xml.text qmax }
       }
     end
   end
--- lib/db/export/node.rb
+++ lib/db/export/node.rb
@@ -3,6 +3,28 @@ module Aurora
     def build_xml(xml)
       xml.node(:id => id, :name => name, :type => self[:type]) {
         xml.description description unless description.empty?
+        
+        unless outputs.empty?
+          outputs_in_order = outputs.sort_by {|output| output.begin_order}
+          xml.outputs {
+            outputs_in_order.each do |output|
+              xml.output(:link_id => output.id)
+            end
+          }
+        end
+        
+        unless inputs.empty?
+          inputs_in_order = inputs.sort_by {|input| input.end_order}
+          xml.inputs {
+            inputs_in_order.each do |input|
+              xml.input(:link_id => input.id) {
+                wf = input.weaving_factors
+                xml << wf if wf
+              }
+            end
+          }
+        end
+        
         xml.position {
           point_attrs = {
             :lat => lat,
