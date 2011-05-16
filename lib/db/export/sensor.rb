2875142defd27c522a72c96eff66c2ff6d8be966
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-05-16 12:13:25 -0700

    export sensors

--- lib/db/export/sensor.rb
+++ lib/db/export/sensor.rb
@@ -1,8 +1,25 @@
 module Aurora
   class Sensor
     def build_xml(xml)
-      xml.sensor(:id => id, :type => self[:type]) {
+      attrs = {
+        :id             => id,
+        :type           => self[:type],
+        :display_lat    => display_lat,
+        :display_lng    => display_lng,
+        :length         => length,
+        :offset_in_link => offset,
+        :data_id        => data_id,
+        :link_type      => link_type,
+        :vds            => vds,
+        :hwy_name       => hwy_name,
+        :hwy_dir        => hwy_dir,
+        :postmile       => postmile,
+        :lanes          => lanes
+      }
+      
+      xml.sensor(attrs) {
         xml.description description unless description.empty?
+        
         xml.position {
           point_attrs = {
             :lat => lat,
@@ -10,6 +27,10 @@ module Aurora
           }
           point_attrs[:elevation] = elevation if elevation != 0
           xml.point point_attrs
+        }
+        
+        xml.links {
+          xml.text link.id
         }
       }
     end
