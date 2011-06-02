6a76e91b1ce462663fb49d12c38df44a9684f601
Joel VanderWerf <vjoel@users.sourceforge.net>
2011-06-02 14:17:09 -0700

    removed tln from schema

--- lib/db/schema.rb
+++ lib/db/schema.rb
@@ -26,10 +26,7 @@ create_table? :scenarios do
   string      :units
   check       :units => Aurora::UNITS
 
-  foreign_key :tln_id,        :tlns, :null => false
-  foreign_key :network_family_id, :network_families, :null => false
-  foreign_key [:tln_id, :network_family_id], :networks,
-                :key => [:network_id, :id]
+  foreign_key :network_id, :networks, :null => false
 
   foreign_key :initial_condition_set_id,    :initial_condition_sets
   foreign_key :demand_profile_set_id,       :demand_profile_sets
@@ -49,22 +46,12 @@ create_table? :vehicle_types do
   foreign_key :scenario_id, :scenarios, :null => false
 end
 
-# Top-level networks. Rows in this table are one-to-one with rows in the
-# networks table that have parent==null. The main reason for having this
-# table separate is to provide half of the composite primary keys that
-# our foreign keys that reference.
-create_table? :tlns do
-  primary_key :id
-  foreign_key :project_id, :projects
-end
-
 # The next set of tables just keep track of ids. Each id in one of these tables
 # corresponds to a whole family of nodes (or links or ...) that all have the
 # same id, but different network_id. This id is used as the second part of the
 # composite key in the node table. The main reason for these tables is so that
 # tables like split_ratio_profiles have a way to refer to a node family, rather
-# than a specific node, by foreign key. (Actually, this is not true for routes
-# and sensors.)
+# than a specific node, by foreign key.
 #
 # These tables also provides a convenient way to generate new ids that are
 # unique across all nodes. This makes it possible, when pasting from one network
@@ -75,11 +62,6 @@ end
 # either are descendants of a common ancestor by way of copying and pasting or
 # duplication or have been explicitly given a commmon identity by the user or
 # application.
-
-# Note that network_family.id is orthogonal to tln.id.
-create_table? :network_families do
-  primary_key :id
-end
 
 create_table? :node_families do
   primary_key :id
@@ -97,44 +79,9 @@ create_table? :sensor_families do
   primary_key :id
 end
 
-# The next section defines the subcomponents of networks (as opposed to the
-# various profile sets, event sets, and controller sets, which are in the
-# scenario). These tables use a composite key:
-#
-#   (network_id, id)
-#
-# The network_id refers to a unique top level network and its contents. It is
-# shared by all rows created for subnetworks, nodes, links, etc. coming from
-# the xml being imported as part of one network.
-#
-# The id (second half of the composite key) is unique only among elements of the
-# same type within the same top level network. This is so that events that refer
-# to a subnetwork can port to variants, for example. There are many foreign keys
-# that refer to this kind of id; these foreign keys are incomplete and can only
-# reference a row when combined with a network_id. When the reference is
-# internal to the network (e.g. parent_id, begin_id), we just use the same
-# network_id. When the reference is external (e.g. from an event), we
-# combine with the network_id specified by the scenario.
-#
-# Distinctly created (not copied) entities remain distinct in the database, so
-# that they can be copied and pasted alongside each other.
-#
-# See dbweb/doc/subnetworks.txt for details.
-
 create_table? :networks do
-  # Serialized in xml as "network_id", but only in the top level network.
-  foreign_key :network_id, :tlns, :null => false
+  primary_key :id
 
-  # This is often 1 for the top network. But, really, parent_id==nil
-  # is the way to tell whether a network is top.
-  # Serialized in xml as "id".
-  foreign_key :id, :network_families, :null => false
-
-  primary_key [:network_id, :id]
-  
-  integer     :parent_id, :null => true
-  foreign_key [:network_id, :parent_id], :networks, :key => [:network_id, :id]
-  
   text        :name
   text        :description
   float       :dt
@@ -150,19 +97,40 @@ create_table? :networks do
   text        :intersection_cache   ## ditto
 end
 
+create_table? :network_lists do
+  foreign_key :network_id, :networks, :null => false
+  foreign_key :child_id, :networks, :null => false
+end
+
+# The next section defines the subcomponents of networks (as opposed to the
+# various profile sets, event sets, and controller sets, which are in the
+# scenario). These tables use a composite key:
+#
+#   (network_id, id)
+#
+# The id (second half of the composite key) is unique only among elements of the
+# same type within the same network. This is so that events that refer
+# to a subnetwork can port to variants, for example. There are many foreign keys
+# that refer to this kind of id; these foreign keys are incomplete and can only
+# reference a row when combined with a network_id. When the reference is
+# internal to the network (e.g. parent_id, begin_id), we just use the same
+# network_id. When the reference is external (e.g. from an event), we
+# need to combine it with a network_id (specified by the scenario's network,
+# or a subnetwork of that).
+#
+# Distinctly created (not copied) entities remain distinct in the database, so
+# that they can be copied and pasted alongside each other.
+#
+# See dbweb/doc/subnetworks.txt for details.
+
 create_table? :nodes do
-  # This network_id is of the top network to which the node belongs, not
-  # the immediate parent network. This is a global ID which changes when
-  # pasting the node into a different network. Not serialized in xml.
-  foreign_key :network_id, :tlns, :null => false
+  # This network_id is the id of the network to which the node belongs. This is
+  # ID changes when pasting the node into a different network. Serialized in xml
+  # implicitly using the hierarchy.
+  foreign_key :network_id, :networks, :null => false
   foreign_key :id, :node_families, :null => false
   primary_key [:network_id, :id]
 
-  # Parent network to which the node belongs. Preserved when pasting the
-  # node. Serialized in xml implicitly using the hierarchy.
-  integer     :parent_id, :null => false
-  foreign_key [:network_id, :parent_id], :networks, :key => [:network_id, :id]
-  
   text        :name
   text        :description
   text        :type_node
@@ -174,12 +142,9 @@ create_table? :nodes do
 end
 
 create_table? :links do
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
   foreign_key :id, :link_families, :null => false
   primary_key [:network_id, :id]
-
-  integer     :parent_id, :null => false
-  foreign_key [:network_id, :parent_id], :networks, :key => [:network_id, :id]
   
   text        :name
   text        :description
@@ -210,18 +175,15 @@ create_table? :links do
 end
 
 create_table? :routes do
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
   foreign_key :id, :route_families, :null => false
   primary_key [:network_id, :id]
-  
-  integer     :parent_id, :null => false
-  foreign_key [:network_id, :parent_id], :networks, :key => [:network_id, :id]
   
   string      :name
 end
 
 create_table? :route_links do
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
   integer     :route_id, :null => false
   integer     :link_id, :null => false
 
@@ -236,12 +198,9 @@ create_table? :route_links do
 end
 
 create_table? :sensors do
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
   foreign_key :id, :sensor_families, :null => false
   primary_key [:network_id, :id]
-
-  integer     :parent_id, :null => false
-  foreign_key [:network_id, :parent_id], :networks, :key => [:network_id, :id]
 
   text        :description
 
@@ -280,42 +239,42 @@ create_table? :split_ratio_profile_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 create_table? :capacity_profile_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 create_table? :demand_profile_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 create_table? :initial_condition_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 create_table? :event_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 create_table? :controller_sets do
   primary_key :id
   string      :name
   text        :description
-  foreign_key :network_id, :tlns, :null => false
+  foreign_key :network_id, :networks, :null => false
 end
 
 # Note on profile, event, and controller tables:
