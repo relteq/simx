498ed683ceab3ee9c31e14628b166ceb6539bbfd
Oliver Newland <onewland@gmail.com>
2011-06-10 11:48:07 -0700

    Removed debug print

--- lib/db/import/model.rb
+++ lib/db/import/model.rb
@@ -1,7 +1,6 @@
 module Aurora
   module Model
     def after_create
-      p 'after_create entered'
       auto_time_fields = [:created_at, :updated_at]
       auto_time_fields.each do |atf|
         if self.class.columns.include?(atf)
