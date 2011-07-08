fe2b08874690eb992fa985e61c9301f57c2b06c2
Oliver Newland <onewland@gmail.com>
2011-07-07 19:10:54 -0700

    Handle reporting job failure to Redmine

--- runq/lib/redmine-callbacks.rb
+++ runq/lib/redmine-callbacks.rb
@@ -31,18 +31,22 @@ module Runq
         		  "#{simulation_batch_id} to #{percent}"
 
         frontend_batch.update( :percent_complete => percent )
-        log.debug "Adding output file for run by worker" +
-        		  "#{req.worker_id} to output_files"
 
-        if req.data['output_urls']
-          req.data['output_urls'].each do |url|
-            dbweb_db[:output_files] << {
-              :simulation_batch_id => simulation_batch_id,
-              :url => url,
-              :created_at => Time.now,
-              :updated_at => Time.now
-            }
+        if req.data['ok']
+          frontend_batch.update( :succeeded => true )
+          if req.data['output_urls']
+            req.data['output_urls'].each do |url|
+              dbweb_db[:output_files] << {
+                :simulation_batch_id => simulation_batch_id,
+                :url => url,
+                :created_at => Time.now,
+                :updated_at => Time.now
+              }
+            end
           end
+        else
+          frontend_batch.update( :succeeded => false, 
+                                 :failure_message => req.data['error'] )
         end
       end
   	end
@@ -55,22 +59,27 @@ module Runq
                         where(:id => batch_param[:redmine_batch_report_id])
       if frontend_report.count > 0
         frontend_report.update(:percent_complete => 1)
-        batch_param['output_types'].each_with_index do |type,index|
-          if frontend_report.count > 0
-            log.info "Setting report export URL for #{type} in Redmine database"
-            ext = ext_for_mime_type(type)
-            field = case ext
-              when "xml" then :url 
-              when "pdf" then :export_pdf_url
-              when "xls" then :export_xls_url
-              when "ppt" then :export_ppt_url
-              else begin 
-                log.warn "Unrecognized extension #{ext} in report generator"
-                break
+        if req.data['ok']
+          batch_param['output_types'].each_with_index do |type,index|
+            if frontend_report.count > 0
+              log.info "Setting report export URL for #{type} in Redmine database"
+              ext = ext_for_mime_type(type)
+              field = case ext
+                when "xml" then :url 
+                when "pdf" then :export_pdf_url
+                when "xls" then :export_xls_url
+                when "ppt" then :export_ppt_url
+                else begin 
+                  log.warn "Unrecognized extension #{ext} in report generator"
+                  break
+                end
               end
+              frontend_report.update(field => req.data['output_urls'][index])
             end
-            frontend_report.update(field => req.data['output_urls'][index])
           end
+        else
+          frontend_report.update( :succeeded => false,
+                                  :failure_message => req.data['error'] )
         end
       end
   	end
