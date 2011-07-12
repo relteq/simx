22f2b0f85657dd04b7067ab2380a7ba8511ba94a
Oliver Newland <onewland@gmail.com>
2011-07-12 11:05:38 -0700

    InitialCondition set copy for Network

--- lib/db/model/initial-condition-set.rb
+++ lib/db/model/initial-condition-set.rb
@@ -6,6 +6,21 @@ module Aurora
     one_to_many :scenarios, :key => :ic_set_id
     one_to_many :initial_conditions, :key => :initial_condition_set_id
 
+    def shallow_copy
+      ics = InitialConditionSet.new
+      ics.columns.each do |c|
+        ics.set(c => self[c]) if c != :id
+      end
+      ics.save
+
+      initial_conditions.each do |ic|
+        icopy = ic.copy
+        icopy.initial_condition_set_id = ics.id
+        icopy.save
+      end
+      return ics
+    end
+
     def clear_members
       initial_conditions.each do |ic|
         ic.destroy
--- lib/db/model/initial-condition.rb
+++ lib/db/model/initial-condition.rb
@@ -2,5 +2,13 @@ module Aurora
   class InitialCondition
     many_to_one :initial_condition_set
     many_to_one :link_family, :key => :link_id
+
+    def copy
+      ic = InitialCondition.new
+      ic.columns.each do |c|
+        ic.set(c => self[c]) if c != :id
+      end
+      return ic
+    end
   end
 end
