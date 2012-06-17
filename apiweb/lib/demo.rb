8a28fd8cbe00c8402f8bf5410646d4ae5c912ce3
Joel VanderWerf <vjoel@users.sourceforge.net>
2012-06-17 15:24:40 -0700

    new demo code, ep 2

--- apiweb/lib/demo.rb
+++ apiweb/lib/demo.rb
@@ -1,10 +1,9 @@
+require 'apiweb/demo_helpers'
+
 get '/demo' do
   @demo_swf = "/demo/Demo.swf"
-
   @scenario_url = "/demo/US101-test.xml"
-  
   @gmap_key = ENV["GMAP_KEY"]
-
   haml(:demo)
 end
 
@@ -13,37 +12,42 @@ get '/demo/density-feed' do
     File.open("public/demo/density-trace.yaml") do |f|
       $data = YAML.load(f)
     end
-    $prev = nil
+    $prev_dmap_true = nil
   end
-
+  ## this should be per session
   
-  ts, dmap1 = $data.shift
-  if not $prev
-    $prev = dmap1
+  ts, dmap_true = $data.shift
+  if not $prev_dmap_true
+    $prev_dmap_true = dmap_true
   end
   
-  dmap2 = {}
-  dmap1.each do |lid,den|
+  dmap_est = {}
+  $prev_dmap_true.each do |lid,den|
     if rand < 0.1
-      dmap2[lid] = 0
+      dmap_est[lid] = 0
       # poor man's fault model
     else
-      dmap2[lid] = den * (1 + (rand - 0.5) * 0.1)
+      dmap_est[lid] = den * (1 + (rand - 0.5) * 0.1)
       # poor man's gaussian noise
     end
   end
 
-  $prev = dmap1
+  $prev_dmap_true = dmap_true
   # poor man's delay
+  
+  # start work on prediction for future use
+  request_predicted_density_from_leia(dmap_est, ts, 5*60)
+  
+  # see if prediction for current time is available yet
+  dmap_pred = read_density_from_leia(ts) || {}
   
   result =
     {
       "tmc_clock_time" => ts,
       "points" => {
-        '1' => dmap1,
-        '2' => dmap2,
-        '3' => {
-        }
+        '1' => dmap_true,
+        '2' => dmap_est,
+        '3' => dmap_pred
       }
     }
   
--- apiweb/lib/demo_helpers.rb
+++ apiweb/lib/demo_helpers.rb
@@ -1,41 +1,111 @@
 require 'rinda/rinda'
 
+# client to talk to a leia server running at a given uri
 class Leia
   def initialize uri
-    DRb.start_service ## idempotent?
-    Rinda::TupleSpaceProxy.new(DRbObject.new(nil, uri))
+    DRb.start_service unless DRb.primary_server ## ok?
+    
+    @proxy = Rinda::TupleSpaceProxy.new(DRbObject.new(nil, uri))
+    @cache = {}
+  end
+  
+  def cached? ts
+    @cache.key? ts
+  end
+  
+  def [](ts)
+    @cache[ts]
+  end
+  
+  def cache! ts, dmap
+    if dmap
+      @cache[ts] = dmap
+    end
+  end
+  
+  def read(*args)
+    @proxy.read(*args)
+  end
+  
+  def read_all(*args)
+    @proxy.read_all(*args)
+  end
+  
+  def write(*args)
+    @proxy.write(*args)
+  end
+  
+  def take(*args)
+    @proxy.take(*args)
   end
 end
 
 helpers do
   def leia
-    LEIA ||= Leia.new(ENV['DEMO_RINDA_URI'] || "druby://localhost:6789"
+    unless $LEIA
+      uri = ENV['DEMO_RINDA_URI'] || "druby://localhost:6789"
+      log.info "connecting to leia server at #{uri}"
+      begin
+        $LEIA = Leia.new(uri)
+        log.info "connected to leia server at #{uri}"
+      rescue => ex
+        log.warn "cannot connect to leia server at #{uri}: #{ex}; retrying..."
+        sleep 5
+        retry
+      end
+    end
+    $LEIA
   end
   
   def read_density_from_leia ts
-    $leia_cache ||= {}
-    
-    if $leia_cache[ts]
-      return $leia_cache[ts]
+    if leia.cached? ts
+      log.debug "got cached data for t = #{ts}: #{leia[ts].inspect}"
+      return leia[ts]
     end
+
+    response =
+      begin
+        log.info "retreiving density prediction for t = #{ts}"
+        leia.read_all([:density_response, ts, Hash])
+      rescue => e
+        log.warn "leia read error: #{e}"
+        sleep 2
+        retry
+      end
     
-    begin
-      _, _, dmap = LEIA.read([:density_response, ts, Hash])
-    rescue => e
-      log.warn "leia read error: #{e}"
-      sleep 2
-      retry
-    end
-    
+    _, _, dmap = response.first
     if dmap
-      $leia_cache[ts] = dmap)
+      log.info "received density prediction for t = #{ts}"
+      leia.cache! ts, dmap
+    else
+      log.warn "density prediction not available for t = #{ts}"
     end
+      
     return dmap
   end
   
-  def request_predicted_density_from_leia d_map, ts_start, ts_delta = 5*60
+  def request_predicted_density_from_leia dmap, ts_start, ts_delta
+    ts_end = ts_start + ts_delta
+    
+    log.debug "checking local cache for t = #{ts_end}"
+    return if leia.cached?(ts_end)
+    
+    log.debug "checking remote cache for t = #{ts_end}"
+    dmap_responses = leia.read_all([:density_response, ts_end, Hash])
+
+    log.debug {
+      "checked remote cache for t = #{ts_end};" +
+      " found #{dmap_responses.size} entries"
+    }
+
+    if not dmap_responses.empty?
+      leia.cache! ts_end, dmap_responses[0][2]
+      return
+    end
+
     begin
-      LEIA.write([:density_prediction_request, ts_start, ts_delta, d_map])
+      log.info "requesting density prediction for t = #{ts_start} + #{ts_delta}"
+      leia.write([:density_prediction_request, ts_start, ts_delta, dmap])
     rescue => e
       log.warn "leia write error: #{e}"
       sleep 2
